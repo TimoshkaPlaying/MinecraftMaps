c159cd4248ac312fc2ffcbea81f509136a415a4e
TimoshkaPlaying <timpug@mail.ru>
2025-02-23 19:00:12 +0300

    It can work. But not all of my dreams

--- map.js
+++ map.js
@@ -2,12 +2,13 @@ let downloadButton = document.querySelector('.downloadButton')
 let downloadLink = document.querySelector('.downloadLink')
 let body = document.querySelector('body')
 
-right_arrow = document.querySelector(".right")
-left_arrow = document.querySelector(".left")
-img = document.querySelector(".active")
-images = ["src/slider1.png", "src/slider2.png", "src/slider3.png"]
-img_now = 0
-button = document.querySelector('button')
+let right_arrow = document.querySelector(".right")
+let left_arrow = document.querySelector(".left")
+let img = document.querySelector(".active")
+// let images = ["src/slider1.png", "src/slider2.png", "src/slider3.png"]
+let images = []
+let img_now = 0
+let button = document.querySelector('button')
 
 // -----------------------------------------------------------------
 
@@ -34,19 +35,67 @@ downloadLink.addEventListener('click', function(event){
 
 
 
-right_arrow.addEventListener("click", function(){
-    img_now += 1
-    if (img_now > (images.length - 1)) {
-        img_now = 0
+// right_arrow.addEventListener("click", function(){
+//     img_now += 1
+//     if (img_now > (images.length - 1)) {
+//         img_now = 0
+//     }
+//     img.src = images[img_now]
+// })
+
+// left_arrow.addEventListener("click", function(){
+//     img_now -= 1
+//     if (img_now == -1) {
+//         img_now = images.length - 1
+//     }
+//     img.src = images[img_now]
+// })
+
+
+function updateSlider() {
+    if (img && images.length > 0) {
+        img.src = images[img_now]
     }
-    img.src = images[img_now]
+}
+
+left_arrow.addEventListener("click", function() {
+    img_now = (img_now - 1 + images.length)%images.length
+    updateSlider()
 })
 
-left_arrow.addEventListener("click", function(){
-    img_now -= 1
-    if (img_now == -1) {
-        img_now = images.length - 1
-    }
-    img.src = images[img_now]
+right_arrow.addEventListener("click", function() {
+    img_now = (img_now + 1)%images.length
+    updateSlider()
 })
 
+document.addEventListener("DOMContentLoaded", function() {
+    let params = new URLSearchParams(window.location.search)
+    let cardName = params.get("card")
+    console.log(cardName)
+    if (!cardName) {
+        console.error("Ошибка! Карта не найдена!")
+        return
+    }
+    fetch("options.json")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            let card = data.find(item => item.cardName === cardName)
+            if (!card) {
+                console.error("Ошибка! Карта не найдена в options.json!")
+                return
+            }
+            document.querySelector(".cardName").textContent = card.cardName
+            document.querySelector(".cardMoreImage").src = card.cardMoreImage
+            document.querySelector(".cardMoreDecription").textContent = card.cardMoreDescription
+            images = card.cardSlider
+            document.querySelector(".downloadButton").dataset.url = card.downloadFile
+            document.querySelector(".downloadLink").textContent = card.version
+            document.querySelector(".downloadLink").href = card.downloadFile
+            document.querySelector(".cardSize").textContent = card.size
+            updateSlider()
+        })
+        .catch(error => {
+            console.error("Ошибка загрузки данных!", error)
+        })
+})
--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// Здесь будет вся логика проекта
+// // Здесь будет вся логика проекта
 
-let link = document.querySelector(".toMapLink")
-let cardName = document.querySelector(".cardName")
-let cardMore = document.querySelector(".cardMore")
+// let link = document.querySelector(".toMapLink")
+// let cardName = document.querySelector(".cardName")
+// let cardMore = document.querySelector(".cardMore")
 
-console.log(link)
+// console.log(link)
 
-link.addEventListener("click", function(){
-    fetch("options.json").then(response=>{
-        if (!response.ok) {
-            throw new Error('Ответ не ок!!')
-        }
-        return response.json()
-    })
-    .then(data=>{
-        cardName.innerHTML = ''
-        data.forEach(element => {
-            let div = document.createElement('div')
-            div.classList.add("cardName")
-            div.innerHTML = `<h3>${element.cardName}<\h3>`
-            cardMore.appendChild(div)
-        })
-    })
-    .catch(error=>{
-        console.log('Error', error)
-    })
-})
+// link.addEventListener("click", function(){
+//     fetch("options.json").then(response=>{
+//         if (!response.ok) {
+//             throw new Error('Ответ не ок!!')
+//         }
+//         return response.json()
+//     })
+//     .then(data=>{
+//         cardName.innerHTML = ''
+//         data.forEach(element => {
+//             let div = document.createElement('div')
+//             div.classList.add("cardName")
+//             div.innerHTML = `<h3>${element.cardName}<\h3>`
+//             cardMore.appendChild(div)
+//         })
+//     })
+//     .catch(error=>{
+//         console.log('Error', error)
+//     })
+// })
