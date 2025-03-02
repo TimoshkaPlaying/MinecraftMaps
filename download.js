c47fe9f0b57f57bca8d86b3b5ad4fc1a71ad34b0
TimoshkaPlaying <timpug@mail.ru>
2025-03-02 20:01:47 +0300

    I'm learning how I can work with server part

--- download.js
+++ download.js
@@ -1,19 +1,39 @@
 document.getElementById("uploadForm").addEventListener("submit", function(event) {
     event.preventDefault()
-    const cardName = document.querySelector(".cardName").value.trim()
+    const cardVersion = document.querySelector(".cardVersion").value
+    const cardName = document.querySelector(".cardName").value.trim() + " " + `(${cardVersion})`
     const cardDescription = document.querySelector(".cardDescription").value.trim()
-    const mainImage = ''
-    const images = ''
-    const map = ''
+    const cardMoreDescription = document.querySelector(".cardMoreDescription").value.trim()
+    const cardImage = document.querySelector(".cardMainImage").files[0]
+    const cardSlider = document.querySelector(".cardImages").files
+    const map = document.querySelector(".cardFile").files[0]
+    let name = cardName.replace(" ", "+")
+    name = name.replace("(", "%28")
+    name = name.replace(")", "%29")
+    const cardHref = `map.html?card=${name}`
+    const cardSize = (map["size"]/1048576).toFixed(1)
 
-    if (!mainImage || !images || !map) {
-        alert("Заполните все поля")
-        return
-    }
+    // if (!cardName || !cardDescription || !cardMoreDescription  || !cardImage || !cardSlider || !map || !cardVersion) {
+    //     alert("Заполните все поля")
+    //     return
+    // }
 
     const formData = new FormData()
     formData.append("cardName", cardName)
+    formData.append("cardHref",  cardHref)
+    formData.append("cardImage", cardImage)
     formData.append("cardDescription", cardDescription)
+    formData.append("cardMoreDescriprion", cardMoreDescription)
+    // formData.append("cardSlider", cardSlider)
+    for (let i=0; i<cardSlider.length; i++) {
+        formData.append("cardSlider", cardSlider[i])
+    }
+    formData.append("version", cardVersion)
+    formData.append("size", cardSize)
+    formData.append("downloadFile", map)
+    formData.append("editor", "false")
+
+
 
     fetch("http://localhost:2550/download", {
         method: "POST",
--- server.js
+++ server.js
@@ -10,9 +10,45 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static("public"))
 
-app.get('/start', (request, response) => {
-    response.json({message:"Привет!"})
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let uploadDir;
+
+       
+        if (file.mimetype === "application/zip" || file.originalname.endsWith(".zip")) {
+            uploadDir = "maps/";
+        } else {
+            uploadDir = "src/";
+        } 
+
+       
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir, { recursive: true });
+        }
+
+        cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+
+app.post("/download", upload.fields([{name:"cardImage"}, {name:"cardSlider"}, {name:"map"}]), (req, res) => {
+    const {cardName, cardHref, cardDescription, cardMoreDescription, cardVersion, cardSize} = req.body
+
+    if (!cardName, !cardHref, !cardDescription, !cardMoreDescription, !cardVersion, !cardSize) {
+        return res.status(400).json({message:"Все поля должны быть заполнены!"})
+    }
+
+    const newCard = {cardName, cardHref, cardDescription, cardMoreDescription, cardVersion, cardSize, 
+        cardImage:req.files["cardImage"] ? req.files["cardImage"][0].path : null,
+        cardSlider:req.files["cardSlider"] ? req.files["cardSlider"].map(file => file.path) : null, 
+        map:req.files["downloadFile"] ? req.files["downloadFile"][0].path : null}
 })
+
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен по адресу http://localhost:${PORT}`)
